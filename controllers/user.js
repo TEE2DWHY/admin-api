6a7e14b06ad8c723d38e24964cea1d39353fc701
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-11-01 23:30:17 +0100

    refactored user.js

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,11 @@ const User = require("../model/User");
 const openDashBoard = asyncWrapper(async (req, res) => {
   const id = req.user.id;
   const user = await User.findOne({ _id: id });
+  if (!user) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      msg: "User not found.",
+    });
+  }
   res.status(StatusCodes.OK).json({
     msg: "Welcome to AppStack Dashboard",
     name: user.firstName,
